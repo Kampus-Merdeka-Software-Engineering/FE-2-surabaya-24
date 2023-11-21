6269d1d2fd14be3b7791c7e983f2b86469b64961
diaharishma <diaharishma@gmail.com>
2023-11-21 18:23:13 +0800

    add foto anggota

--- Product/ProductJS.js
+++ Product/ProductJS.js
@@ -4,3 +4,20 @@ function expandText(button) {
     content.classList.toggle("expanded");
     button.innerHTML = content.classList.contains("expanded") ? "Read Less" : "Read More";
 }
+
+// feedback
+const allStars = document.querySelectorAll('.star');
+console.log(allStars);
+allStars.forEach( (star,i))=>{
+    star.onclick = function(){
+        let.current_star_level = i + 1;
+
+        allStars.forEach(star, j)=> {
+            if(current_star_level >= j + 1){
+                star.innerHTML = '&#9734';
+            } else{
+                star.innerHTML = '&#9734'
+            }
+        }
+    }
+}
