64e89e8a678228a46fb75f97eef6e880e8e1c9de
rifqnurp <rifqn14@gmail.com>
2023-11-22 12:43:23 +0700

    Update tabContent Product
    - Perubahan tabContent menu product
    - Membuat backup data products sebelumnya

--- Product/ProductJS.js
+++ Product/ProductJS.js
@@ -1,3 +1,21 @@
+// Tab Content pada Menu Product
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
 // Read More Product
 function expandText(button) {
     var content = button.previousElementSibling;
