f82b98dca6f47f53d388f630bb29c5ee2e29df22
Azzizah01 <azzizahnur01@gmail.com>
2023-12-11 12:57:05 +0700

    remove code

--- JS/ComponentGetFeddback.js
+++ JS/ComponentGetFeddback.js
@@ -1,17 +1,18 @@
 export function createFeed(Order) {
+  if (!Order || typeof Order !== "object" || !Order.feedbackName) {
+    return "";
+  }
+
   const cardFeed = `
-        <div class="card-spill" id="feedback-form">
-            <div class="card-body">
-                <img src="${Order.feedbackImg}" alt="people1" />
-                <h6>${Order.feedbackName}</h6>
-                <div class="text-card">
-                  <p>
-                  ${Order.feedbackComment}
-                  </p>
-                </div>
-            </div>
+    <div class="card-spill" id="feedback-form">
+      <div class="card-body">
+        <h6>${Order.feedbackName}</h6>
+        <div class="text-card">
+          <p>${Order.feedbackComment}</p>
         </div>
-    `;
+      </div>
+    </div>
+  `;
 
   return cardFeed;
 }
--- JS/GetFeed.js
+++ JS/GetFeed.js
@@ -6,44 +6,18 @@ document.addEventListener("DOMContentLoaded", async () => {
     const DataFeed = await getFeedback();
 
     const feedbacksCard = document.getElementById("feedback-form");
-    const feedbacksCard1 = document.getElementById("feedbacks-container2");
-    const feedbacksCard2 = document.getElementById("feedbacks-container3");
 
     if (DataFeed.length > 0) {
       let feedbacksCards = "";
 
-      for (let i = 0; i < 9; i++) {
+      for (let i = 0; i < 3; i++) {
         feedbacksCards += createFeed(DataFeed[i]);
       }
 
       feedbacksCard.innerHTML = feedbacksCards;
 
       console.log("Feedback", DataFeed[0].feedbackName);
-    }
-
-    if (DataFeed.length > 0) {
-      let feedbacksCards1 = "";
-
-      for (let i = 0; i < 9; i++) {
-        feedbacksCards1 += createFeed(DataFeed[i]);
-      }
-
-      feedbacksCard1.innerHTML = feedbacksCards1;
-
-      console.log("Feedback", DataFeed[0].feedbackName);
-    }
-
-    if (DataFeed.length > 0) {
-      let feedbacksCards2 = "";
-
-      for (let i = 0; i < 3; i++) {
-        feedbacksCards2 += createFeed(DataFeed[i]);
-      }
-
-      feedbacksCard2.innerHTML = feedbacksCards2;
-
-      console.log("Feedback", DataFeed[0].feedbackName);
-    } else {
+    }else {
       console.error("No feedback data found or an error occurred.");
     }
   } catch (error) {
